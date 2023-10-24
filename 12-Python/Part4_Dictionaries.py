58c7f76e50634c130de991b491508480c561346c
Nick <nicholasan98@gmail.com>
2023-10-23 20:11:23 -0400

    update 2 for the day

---
+++ 12-Python/Part4_Dictionaries.py
@@ -0,0 +1,9 @@
+# Dictionaries
+my_stuff = {"key1":"value", 'key2':'value2', 'key3':{'123':[1,2,'grabMe']}}
+print(my_stuff['key3']['123'][2])
+
+my_things = {'lunch':'pizza', 'bfast':'eggs'}
+my_things['lunch'] = 'burger'
+my_things['dinner'] = 'pasta'
+
+print(my_things)
---
+++ 12-Python/Part5_TuplesSets&Booleans.py
@@ -0,0 +1,17 @@
+# Tuples - like lists, but they are immutable
+t = (1, 2, 3)
+print(t[0])
+
+t = ('a', True, 123)
+print(t)
+
+# t[0] = 'NEW'
+
+# Sets - ordered unique aspects
+x = set()
+
+x.add(1)
+x.add(2)
+x.add(4)
+x.add(0.1)
+print(x)
